35f69e1d819fb69ed61763331aa6c22c234c2a9c
vansssa <sqa-pt@KKBOXdeMacBook-Air-80.local>
2015-12-22 18:05:57 +0800

    edit solution: sum all answer

--- assignment3.py
+++ assignment3.py
@@ -2,20 +2,21 @@
 def sorted_str_and_sum(file):
 	
 	with open(file,'r') as f :
-		 return_result=[]
+		 return_result = 0
 		 for line in f :
-			li=line.replace("\"","").split(',')
-			for l in sorted(li) :
-				result=0
+			li = sorted(line.replace("\"","").split(','))
+			for l in li :
+				result = 0
 			 	for i in range(0,len(l),1) :
-			 		result =result + (ord(l[i:i+1])-64)
-			 	
-			 	return_result=return_result+[result]
-			
+			 		result = result + (ord (l[i : i + 1 ]) - 64)
+				
+			 	result = result * (int((li.index(l))) + 1)
+			 	return_result = result + return_result
+				
 			return return_result
 
 
-sorted_str_and_sum('a2.txt')
+print sorted_str_and_sum('a2.txt')
 
 
 
