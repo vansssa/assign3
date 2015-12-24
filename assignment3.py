7768e1648005d9482c630395b75816cbbc042441
vansssa <sqa-pt@KKBOXdeMacBook-Air-80.local>
2015-12-24 09:58:23 +0800

    調整

--- assignment3.py
+++ assignment3.py
@@ -1,23 +1,19 @@
 
 def sorted_str_and_sum(file):
-	
-	with open(file,'r') as f :
-		 return_result = 0
-		 for line in f :
-			li = sorted(line.replace("\"","").split(','))
-			for l in li :
-				result = 0
-			 	for i in range(0,len(l),1) :
-			 		result = result + (ord (l[i : i + 1 ]) - 64)
-				
-			 	result = result * (int((li.index(l))) + 1)
-			 	return_result = result + return_result
-				
-			return return_result
+
+    with open(file, 'r') as f:
+        return_result = 0
+        for line in f:
+            li = sorted(line.replace("\"", "").split(','))
+            for l in li:
+                result = 0
+                for i in range(0, len(l), 1):
+                    result = result + (ord(l[i: i + 1]) - 64)
+
+                result = result * (int((li.index(l))) + 1)
+                return_result = result + return_result
+
+            return return_result
 
 
 print sorted_str_and_sum('a2.txt')
-
-
-
-
